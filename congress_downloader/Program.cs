a3fb9e73a07cd5bd91cb2a8992f00886d61d81a8
Phill <phillip.vanhoven@gmail.com>
2020-08-01 21:09:48 -0600

    Refactored congress downloader to break up main

--- congress_downloader/Program.cs
+++ congress_downloader/Program.cs
@@ -7,12 +7,12 @@ using Newtonsoft.Json;
 
 namespace congress_downloader
 {
-    class Program
+    public class Program
     {
         const string SENATE_URL = "https://www.senate.gov/legislative/LIS/roll_call_lists/vote_menu_{0}_{1}.xml";
         const string roll_call = "http://www.senate.gov/legislative/LIS/roll_call_votes/vote{0}{1}/vote_{0}_{1}_{2}.xml";
 
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
             Console.WriteLine("Starting...");
 
@@ -28,56 +28,91 @@ namespace congress_downloader
             HttpClient client = new HttpClient();
             foreach (var congress in congresses)
             {
-                foreach (var session in sessions)
+                ProcessSessions(client, congress, sessions, dataDir);
+            }
+        }
+
+        public static void ProcessSessions(HttpClient client, int congress, int[] sessions, string dataDir)
+        {
+            foreach (var session in sessions)
+            {
+                var response = GetSummaryDataResponse(client, congress, session);
+                Console.WriteLine($"Response: {response.StatusCode}");
+                string directoryName = $"{dataDir}/{congress}/{session}";
+                if (!Directory.Exists(directoryName))
                 {
-                    string summaryUrl = string.Format(SENATE_URL, congress, session.ToString());
-                    Console.WriteLine($"Summary Url: {summaryUrl}");
-
-                    var response = client.GetAsync(summaryUrl).Result;
-                    Console.WriteLine($"Response: {response.StatusCode}");
-                    string directoryName = $"{dataDir}/{congress}/{session}";
-                    if (!Directory.Exists(directoryName))
-                    {
-                        Directory.CreateDirectory(directoryName);
-                    }
-
-                    string summaryFileName = $"{directoryName}/summary.xml";
-
-                    using(var fileStream = File.Create(summaryFileName))
-                    {
-                        Stream responseContentStream = response.Content.ReadAsStreamAsync().Result;
-                        responseContentStream.Seek(0, SeekOrigin.Begin);
-                        responseContentStream.CopyTo(fileStream);
-                    }
-
-                    using(StreamReader reader = new StreamReader(summaryFileName))
-                    {
-                        string fileContent = reader.ReadToEnd();
-                        XDocument doc = XDocument.Parse(fileContent);
-                        string json = JsonConvert.SerializeXNode(doc);
-                        dynamic data = JsonConvert.DeserializeObject<ExpandoObject>(json);
-
-                        Console.WriteLine($"congression: {congress}, session: {session} data: {data}");
-                        foreach (var vote in data.vote_summary.votes)
-                        {
-                            Console.WriteLine($"Vote: {vote}");
-                            foreach (var el in vote.Value)
-                            {
-                                string voteUrl = string.Format(roll_call, congress, session, el.vote_number);
-                                Console.WriteLine($"Url: {voteUrl}");
-                                var voteResponse = client.GetAsync(voteUrl).Result;
-                                string voteFileName = $"{directoryName}/{el.vote_number}";
-
-                                using(var fileStream = File.Create(voteFileName))
-                                {
-                                    Stream responseContentStream = voteResponse.Content.ReadAsStreamAsync().Result;
-                                    responseContentStream.Seek(0, SeekOrigin.Begin);
-                                    responseContentStream.CopyTo(fileStream);
-                                }
-                            }
-                        }
-                    }
+                    Directory.CreateDirectory(directoryName);
                 }
+
+                string summaryFileName = $"{directoryName}/summary.xml";
+                CreateSummaryFile(summaryFileName, response);
+
+                var data = GetSummaryData(summaryFileName);
+                Console.WriteLine($"congression: {congress}, session: {session} data: {data}");
+                foreach (var vote in data.vote_summary.votes)
+                {
+                    ProcessVotes(vote, directoryName, congress, session, client);
+                }
+            }
+        }
+
+        public static HttpResponseMessage GetSummaryDataResponse(HttpClient client, int congress, int session)
+        {
+            string summaryUrl = string.Format(SENATE_URL, congress, session.ToString());
+            Console.WriteLine($"Summary Url: {summaryUrl}");
+
+            var response = client.GetAsync(summaryUrl).Result;
+            return response;
+        }
+
+        public static void CreateSummaryFile(string summaryFileName, HttpResponseMessage response)
+        {
+            using(var fileStream = File.Create(summaryFileName))
+            {
+                Stream responseContentStream = response.Content.ReadAsStreamAsync().Result;
+                responseContentStream.Seek(0, SeekOrigin.Begin);
+                responseContentStream.CopyTo(fileStream);
+            }
+        }
+
+        public static void ProcessVotes(dynamic vote, string directoryName, int congress, int session, HttpClient client)
+        {
+            Console.WriteLine($"Vote: {vote}");
+            foreach (var el in vote.Value)
+            {
+                var voteResponse = GetVoteResponse(congress, session, el, client);
+                WriteFile(directoryName, voteResponse, el);
+            }
+        }
+
+        public static dynamic GetSummaryData(string summaryFileName)
+        {
+            using(StreamReader reader = new StreamReader(summaryFileName))
+            {
+                string fileContent = reader.ReadToEnd();
+                XDocument doc = XDocument.Parse(fileContent);
+                string json = JsonConvert.SerializeXNode(doc);
+                dynamic data = JsonConvert.DeserializeObject<ExpandoObject>(json);
+                return data;
+            }
+        }
+
+        public static HttpResponseMessage GetVoteResponse(int congress, int session, dynamic el, HttpClient client)
+        {
+            string voteUrl = string.Format(roll_call, congress, session, el.vote_number);
+            Console.WriteLine($"Url: {voteUrl}");
+            var voteResponse = client.GetAsync(voteUrl).Result;
+            return voteResponse;
+        }
+
+        public static void WriteFile(string directoryName, HttpResponseMessage voteResponse, dynamic el)
+        {
+            string voteFileName = $"{directoryName}/{el.vote_number}";
+            using(var fileStream = File.Create(voteFileName))
+            {
+                Stream responseContentStream = voteResponse.Content.ReadAsStreamAsync().Result;
+                responseContentStream.Seek(0, SeekOrigin.Begin);
+                responseContentStream.CopyTo(fileStream);
             }
         }
     }
